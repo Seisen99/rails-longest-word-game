f61f2477e2dbdf25708f286ef8a0f29549dbbab5
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-14 00:46:20 +0100

    cleanbord

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+
+  def new
+    @grid = generate_grid(params[:size]) { ('A'..'Z').to_a.sample }
+  end
+
+  def generate_grid(grid_size)
+    # TODO: generate random grid of letters
+    return Array.new(grid_size.to_i) { ("A".."Z").to_a.sample }
+  end
+
+
+  def score
+    end_time = Time.now.to_i
+    @word = params[:word]
+  end
+
+  def home
+  end
+
+
+end
